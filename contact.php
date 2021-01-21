727e14dfa42d5b821754c3cbeea1ad1357e1decc
Karolina <karolina@nofluffjobs.com>
2021-01-21 16:06:01 +0100

    aaa

--- contact.php
+++ contact.php
@@ -64,7 +64,6 @@ $headers .= "Reply-To: $email" . PHP_EOL;
 $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
-echo($address, $e_subject, $msg, $headers)
 if(mail($address, $e_subject, $msg, $headers)) {
 
 	// Email has sent successfully, echo a success page.
